d33e616346b2264ed2c0825054d6ca8bccde4f86
Daniel <damargon12@gmail.com>
2025-01-31 21:51:55 +0100

    second commit, first try version

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns - [
     path('', views.home, name='home'),
+    path('api/ia/', views.api_ia, name='api_ia'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
+def api_ia(request):
+    respuesta_ia = "Hola, soy una IA respondiendo desde Django."
+    return JsonResponse({"respuesta": respuesta_ia})
